6361ca12d859b4a961d830a518f8323fa35649aa
timontuitoek <tuitoektimon@gmail.com>
2023-03-27 12:54:45 -0700

    function to handle precision

--- precision.c
+++ precision.c
@@ -1,7 +1,5 @@
 #include <stdarg.h>
 #include <stdio.h>
-#include <string.h>
-#include "main.h"
 
 /**
  * get_precision - custom printf function that handles precision for non-custom conversion specifiers
@@ -11,82 +9,53 @@
 int get_precision(const char *format, ...)
 {
 	va_list args;
-	int printed_chars = 0;
-	int precision = (-1);
+	 int printed_chars = 0;
+	 int precision = -1;
 
-	va_start(args, format);
+    va_start(args, format);
 
-	while (*format != '\0')
-	{
-		if (*format == '%')
-		{
-			format++;
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
 
-			/* handle precision for floating-point numbers*/
-			if (*format == '.')
-			{
-				format++;
-				precision = 0;
-				while (*format >= '0' && *format <= '9')
-				{
-					precision = (precision * 10) + (*format - '0');
-					format++;
-				}	
-			}
+ 	if (*format == '.')
+            {
+                precision = 0;
+                while (*(++format) >= '0' && *format <= '9')
+                    precision = precision * 10 + (*format - '0');
+            }
 
-			switch (*format)
-			{
-				case 'd':
-				{
-					int value = va_arg(args, int);
-					printf("%d", value);
-					printed_chars++;
-					break;
-				}
-				case 'f':
-				{
-					double value = va_arg(args, double);
-					if (precision != -1)
-					{
-						printf("%.*f", precision, value);
-					}
-					else
-					{
-						printf("%f", value);
-					}
-					printed_chars++;
-					break;
-				}
-				case 'c':
-				{
-					char value = va_arg(args, int);  /* char is promoted to int in variadic functions*/
-					printf("%c", value);
-					printed_chars++;
-					break;
-				}
-				case 's':
-				{
-					char *value = va_arg(args, char *);
-					printf("%s", value);
-					printed_chars += strlen(value);
-					break;
-				}
-				default:
-					printf("Invalid format specifier");
-					printed_chars += 20; /* assuming 20 characters are printed for an error message*/
-				break;
-			}
-		}
-		else
-		{
-			printf("%c", *format);
-			printed_chars++;
-		}
+            switch (*format)
+            {
+                case 'd':
+                    printed_chars += printf("%d", va_arg(args, int));
+                    break;
+                case 'f':
+                    printed_chars += precision == -1 ? printf("%f", va_arg(args, double))
+                                                      : printf("%.*f", precision, va_arg(args, double));
+                    break;
+                case 'c':
+                    printed_chars += printf("%c", va_arg(args, int));
+                    break;
+                case 's':
+                    printed_chars += printf("%.*s", precision, va_arg(args, char *));
+                    break;
+                default:
+                    printed_chars += printf("Invalid format specifier");
+                    break;
+            }
+        }
+        else
+        {
+            putchar(*format);
+            printed_chars++;
+        }
+        format++;
+    }
 
-		format++;
-	}
+    va_end(args);
 
-	va_end(args);
-
-	return printed_chars;
+    return printed_chars;
 }
