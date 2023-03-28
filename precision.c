fa3070e56581e5d387e855db9ea1af0725027257
timontuitoek <tuitoektimon@gmail.com>
2023-03-28 11:53:22 -0700

    Function to handle precision

--- precision.c
+++ precision.c
@@ -20,29 +20,23 @@ int get_precision(const char *format, int *i, va_list list)
 
 	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
 	{
-		while (is_digit(format[curr_i]))
+		if (is_digit(format[curr_i]))
 		{
 			precision *= 10;
 			precision += format[curr_i] - '0';
-			curr_i++;
 		}
-		 if (format[curr_i] == '*')
+		else if (format[curr_i] == '*')
 		{
 			curr_i++;
-			int arg = va_arg(list, int);
-			if (arg >= 0)
-			{
-				precision = arg;
-			}
+			precision = va_arg(list, int);
 		}
-		else
-		{
+		else{
 			precision = -1;
 		}
 	}
 
 	*i = curr_i - 1;
 
-	return precision;
+	return (precision);
 }
 
